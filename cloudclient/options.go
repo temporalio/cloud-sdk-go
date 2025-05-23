43f16f779fe8f10d9cd91456e5a1cabfbe581e4e
mattkim <matt@temporal.io>
2025-05-23 14:54:22 -0700

    bump cloud go sdk to use cloud api v0.5.1 (#22)

--- cloudclient/options.go
+++ cloudclient/options.go
@@ -17,13 +17,13 @@ import (
 
 const (
 	defaultCloudOpsAPIHostPort = "saas-api.tmprl.cloud:443"
-	defaultAPIVersion          = "v0.5.0"
+	defaultAPIVersion          = "v0.5.1"
 
 	authorizationHeader           = "Authorization"
 	authorizationBearer           = "Bearer"
 	temporalCloudAPIVersionHeader = "temporal-cloud-api-version"
 
-	sdkVersion = "0.3.0"
+	sdkVersion = "0.3.1"
 )
 
 // Options to configure the cloud operations client.
